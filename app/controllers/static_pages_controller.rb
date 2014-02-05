969ee52857f4c8a432ab49212ed8e27567c2cf13
Anand <anand180@gmail.com>
2014-02-05 14:59:57 -0500

    added cache testing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    (1..10000).each do |x|
+      Rails.cache.fetch "key-#{x}" do
+        rand(1..1000)
+      end
+    end
+    
     if signed_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
